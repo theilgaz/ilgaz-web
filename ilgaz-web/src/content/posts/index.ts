3af50daeabf920dd700e028540f4b6f41467d05d
theilgaz <mail@abdullahilgaz.com>
2026-02-21 17:29:59 +0300

    feat(blog): add new post about seeking approval from others
    New reflection piece exploring why we seek validation from others and
    how to find freedom by focusing on what truly matters.

--- ilgaz-web/src/content/posts/index.ts
+++ ilgaz-web/src/content/posts/index.ts
@@ -1,6 +1,7 @@
 import Post1, { frontmatter as meta1 } from './sessizligin-basladigi-yer.mdx'
 import Post2, { frontmatter as meta2 } from './essiz-yaraticinin-izniyle.mdx'
 import Post3, { frontmatter as meta3 } from './yapay-zeka-caginda-yazilimcinin-yeri.mdx'
+import Post4, { frontmatter as meta4 } from './kimin-gozunde-degerli.mdx'
 
 export interface PostMeta {
   slug: string
@@ -16,6 +17,7 @@ export interface Post {
 }
 
 export const posts: Post[] = [
+  { meta: meta4, Content: Post4 },
   { meta: meta3, Content: Post3 },
   { meta: meta2, Content: Post2 },
   { meta: meta1, Content: Post1 },
